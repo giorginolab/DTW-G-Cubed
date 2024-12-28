b21dc204c184a6201343b48b012c745692431f9c
Toni <toni.giorgino@gmail.com>
2024-12-28 15:20:48 +0100

    add study data

--- bad/study.R
+++ bad/study.R
@@ -1,5 +1,6 @@
 load("bad/sw_matrix.RData")
 
+
 image(1:nrow(sw_matrix), 1:ncol(sw_matrix), sw_matrix, useRaster = T, asp=1)
 
 c<-"grey"
@@ -9,3 +10,9 @@ for(i in seq(0,5000,by=200)) {
   abline(a=-i, b=0.5, col=c)
   abline(a=-i, b=2, col=c)
 }
+
+
+load("bad/gcm.RData")
+cm <- gcm$costMatrix
+image(1:nrow(sw_matrix), 1:ncol(sw_matrix), sw_matrix, useRaster = T, asp=1)
+
